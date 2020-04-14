0e7a608e4bb40e2eefb11b8f845d7f8881bfda90
plumest <chayathon.k@ku.th>
2020-04-14 11:44:59 +0700

    Implements ObjectsPool Design Pattern

--- src/BulletsPool.java
+++ src/BulletsPool.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BulletsPool {
+    private static List<Bullet> _available = new ArrayList<Bullet>();
+    private List<Bullet> _inUse = new ArrayList<Bullet>();
+
+    private int counter = 0;
+    private int MAXTotalBullets;
+
     private static BulletsPool instance;
 
     private BulletsPool() {}
@@ -8,5 +17,31 @@ public class BulletsPool {
             instance = new BulletsPool();
         }
         return instance;
+    }
+
+    public Bullet acquireReusable() {
+        if (_available.size() != 0 && _available.size() < 10) {
+            Bullet bullet = _available.get(0);
+            _inUse.add(bullet);
+            _available.remove(0);
+            counter--;
+            return bullet;
+        } else {
+            Bullet bullet = new Bullet(0, 0, 1, 0);
+            _inUse.add(bullet);
+            return bullet;
+        }
+    }
+
+    public void ReleaseReusable(Bullet bullet) {
+        if (counter <= MAXTotalBullets) {
+            _available.add(bullet);
+            counter++;
+            _inUse.remove(bullet);
+        }
+    }
+
+    public void setMAXTotalBullets(int maxBullets) {
+        MAXTotalBullets = maxBullets;
     }
 }
